77df56d5a9b2ab2b31942f468c9c22714868fabe
sayeems <sayeems@gmail.com>
2023-07-28 01:32:32 +0600

    post-thumbnail added to landing page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "dev-non-starter-kit-smart-cache-27-jul-2023.pantheonsite.io",
+      "live-non-starter-kit-smart-cache-27-jul-2023.pantheonsite.io",
+      "test-non-starter-kit-smart-cache-27-jul-2023.pantheonsite.io",
+    ],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,24 @@
 import Layout from "@/components/layout";
+import Image from "next/image";
 export default function Home({ articles }) {
   return (
     <Layout>
-      <div className="grid grid-cols-3 gap-6 pb-20 prose mx-auto">
+      <div className="grid grid-cols-3 gap-6 pb-20 mx-auto px-20">
         {articles.map((article) => {
           return (
             <div
               key={article.id}
-              className="border border-slate-300 rounded-lg overflow-hidden shadow-lg"
+              className="border prose border-slate-300 rounded-lg overflow-hidden shadow-lg"
             >
-              <h4 className="mt-0 p-2">{article.attributes.title}</h4>
+              <Image
+                src={article.thumbnail}
+                alt={article.title}
+                width={100}
+                height={100}
+                layout="responsive"
+                className="my-0 w-full"
+              />
+              <h4 className="mt-0 p-2">{article.title}</h4>
             </div>
           );
         })}
@@ -25,9 +34,11 @@ export async function getServerSideProps({ req, res }) {
     "Cache-Control",
     "public, s-maxage=3000, stale-while-revalidate=600"
   );
+  const backend = `https://dev-non-starter-kit-smart-cache-27-jul-2023.pantheonsite.io`;
+  const sourcePath = `/en/jsonapi/node/article?fields[node--article]=uid,title,changed,field_media_image&include=field_media_image,field_media_image.thumbnail`;
 
-  // fetch data from Drupal CMS
-  const articleEndpoint = `https://dev-non-starter-kit-smart-cache-27-jul-2023.pantheonsite.io/en/jsonapi/node/article`;
+  // fetch data from CMS
+  const articleEndpoint = `${backend}${sourcePath}`;
   const initData = await fetch(articleEndpoint, {
     method: "GET",
     // request header to get the surrogate-key
@@ -41,11 +52,36 @@ export async function getServerSideProps({ req, res }) {
   res.setHeader("surrogate-key", surrogateKey);
 
   const data = await initData.json();
+  const preArticles = data.data;
+  let articles = [];
+
+  // manage image relationship - this part can be ignored
+  preArticles.forEach((article) => {
+    let articleObj = {
+      title: article.attributes.title,
+      id: article.id,
+    };
+    const imageRelation = article.relationships.field_media_image.data;
+    if (imageRelation) {
+      const imageId = imageRelation.id;
+      const imageObject = data.included.find((item) => item.id == imageId);
+      if (imageObject) {
+        const thumbId = imageObject.relationships.thumbnail?.data?.id;
+        if (thumbId) {
+          const thumbObject = data.included.find((item) => item.id == thumbId);
+          const thumbUri = thumbObject.attributes.uri.url;
+          const thumbUrl = `${backend}${thumbUri}`;
+          articleObj.thumbnail = thumbUrl;
+        }
+      }
+    }
+    articles.push(articleObj);
+  });
 
   if (data) {
     return {
       props: {
-        articles: data.data,
+        articles,
       },
     };
   } else {
