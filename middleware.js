de21512b8f2b5166c45d0d20d2777080d1d689ba
sayeems <sayeems@gmail.com>
2023-08-03 21:06:07 +0600

    basic auth

---
+++ middleware.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+
+const BASIC_USER = process.env.BASIC_AUTH_USER;
+const BASIC_PASS = process.env.BASIC_AUTH_PASS;
+
+// Check if authenticated
+export function middleware(req) {
+  if (!isAuthenticated(req)) {
+    return new NextResponse("Authentication required", {
+      status: 401,
+      headers: { "WWW-Authenticate": "Basic" },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+// Check HTTP Basic Auth
+function isAuthenticated(req) {
+  const authheader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
+
+  if (!authheader) {
+    return false;
+  }
+
+  const auth = Buffer.from(authheader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (user == BASIC_USER && pass == BASIC_PASS) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// do not define matcher if you want Basic Auth for all pages
+// export const config = {
+//   matcher: ["/"],
+// };
