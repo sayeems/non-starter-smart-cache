072f1d3c5336db8685d3c2a3769568705f49de8b
sayeems <sayeems@gmail.com>
2023-08-04 00:49:15 +0600

    ip whitelist example

--- middleware.js
+++ middleware.js
@@ -1,44 +1,59 @@
 import { NextResponse } from "next/server";
 
-const [BASIC_USER, BASIC_PASS] = (process.env.HTTP_BASIC_AUTH || ":").split(
-  ":"
-);
+// const [BASIC_USER, BASIC_PASS] = (process.env.HTTP_BASIC_AUTH || ":").split(
+//   ":"
+// );
 
 // Check if authenticated
-export function middleware(req) {
-  if (!isAuthenticated(req)) {
-    return new NextResponse("Authentication required", {
-      status: 401,
-      headers: { "WWW-Authenticate": "Basic" },
-    });
-  }
+// export function middleware(req) {
+//   if (!isAuthenticated(req)) {
+//     return new NextResponse("Authentication required", {
+//       status: 401,
+//       headers: { "WWW-Authenticate": "Basic" },
+//     });
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 // Check HTTP Basic Auth
-function isAuthenticated(req) {
-  const authheader =
-    req.headers.get("authorization") || req.headers.get("Authorization");
+// function isAuthenticated(req) {
+//   const authheader =
+//     req.headers.get("authorization") || req.headers.get("Authorization");
 
-  if (!authheader) {
-    return false;
-  }
+//   if (!authheader) {
+//     return false;
+//   }
 
-  const auth = Buffer.from(authheader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  const user = auth[0];
-  const pass = auth[1];
+//   const auth = Buffer.from(authheader.split(" ")[1], "base64")
+//     .toString()
+//     .split(":");
+//   const user = auth[0];
+//   const pass = auth[1];
 
-  if (user == BASIC_USER && pass == BASIC_PASS) {
-    return true;
-  } else {
-    return false;
-  }
-}
+//   if (user == BASIC_USER && pass == BASIC_PASS) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // do not define matcher if you want Basic Auth for all pages
 // export const config = {
 //   matcher: ["/"],
 // };
+
+// IP checklist
+
+export function middleware(req) {
+  let whiteListedIP = ["127.0.0.1"];
+  let ip = req.headers.get("x-forwarded-for");
+  console.log(ip);
+  const matchIP = whiteListedIP.find((e) => e == ip);
+  if (!matchIP) {
+    return new NextResponse("Access Denied!", {
+      status: 403,
+    });
+  }
+  return NextResponse.next();
+}
